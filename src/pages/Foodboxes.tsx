3f25c908942d1d4f0db7dd58c1c83bf44b0309e8
amir002001 <a_amirhossein@icloud.com>
2023-01-28 20:18:14 -0500

    adds home page change and skeleton for foodboxes

---
+++ src/pages/Foodboxes.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Foodboxes() {
+    return (
+        <>
+            <Head>
+                <title>Foodbox | Foodboxes</title>
+                <meta name="description" content="Foodbox: Dine and Discover" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="container mx-auto w-screen px-6"></main>
+        </>
+    );
+}
